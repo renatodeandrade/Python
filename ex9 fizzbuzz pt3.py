ccff518f179c85a8609b3bee8cbd0196f3b74d33
Renato de Andrade <natores.andrade@gmail.com>
2022-08-23 17:30:45 -0300

    Update ex9 fizzbuzz pt3.py

--- ex9 fizzbuzz pt3.py
+++ ex9 fizzbuzz pt3.py
@@ -1,5 +1,9 @@
+from multiprocessing.resource_sharer import stop
+
+
 num = int(input("Digite um número inteiro: "))
-if (num % 3 and num % 5) == 0:
-    print("FizzBuzz")
-else:
+if num <= 0:
     print(num)
+else:
+    if (num % 3 and num % 5) == 0:
+        print("FizzBuzz")
