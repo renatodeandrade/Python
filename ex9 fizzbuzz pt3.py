78963d646c8c39d0c0dc515b9c5b5dc5a2a45a51
Renato de Andrade <natores.andrade@gmail.com>
2022-08-23 13:01:54 -0300

    Update ex9 fizzbuzz pt3.py

--- ex9 fizzbuzz pt3.py
+++ ex9 fizzbuzz pt3.py
@@ -1,7 +1,6 @@
 num = int(input("Digite um número inteiro: "))
 if num <= 0:
     print(num)
-if (num % 3 and num % 5) == 0:
-    print("FizzBuzz")
 else:
-    print(num)
+    (num % 3 and num % 5) == 0
+    print("FizzBuzz")
