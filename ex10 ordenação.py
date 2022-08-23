f1ed41c6a662867f9fdc621d8043eae302797630
Renato de Andrade <natores.andrade@gmail.com>
2022-08-23 12:25:25 -0300

    .

---
+++ ex10 ordenação.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite um número inteiro: "))
+num3 = int(input("Digite um número inteiro: "))
+if (num1 < num2 and num3):
+    print("crescente")
+else:
+    print("não está na ordem crescente")
