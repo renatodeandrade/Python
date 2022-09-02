3351e37c86164aaa6168fb3267e511f1cbfde543
Renato de Andrade <natores.andrade@gmail.com>
2022-09-02 18:59:35 -0300

    .

---
+++ 22.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome completo: ')
+print(f'O nome em letras maiúsculas é: {nome.upper()}.')
+print(f'O nome em letras minúsculas é: {nome.lower()}.')
+print(f'O nome possui {len(nome.replace(" ", ""))} letras no total.')
--- mp3.py
+++ mp3.py
@@ -1,5 +1,6 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('mp3.mp3')
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.load('alarme.ogg')
 pygame.mixer.music.play()
 pygame.event.wait()
