de6d8826476a495545c2ef5df382d1921606d5c9
Renato de Andrade <natores.andrade@gmail.com>
2022-09-02 22:12:05 -0300

    .

---
+++ maisdeoitomil.py
@@ -0,0 +1,2 @@
+num = int(input('Insira um número de 0 a 9999: '))
+print()
--- mp3.py
+++ mp3.py
@@ -1,6 +1,5 @@
 import pygame
 pygame.init()
-pygame.mixer.music.set_volume(0.2)
-pygame.mixer.music.load('alarme.ogg')
+pygame.mixer.music.load('alarme.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
