4f48ffaa45c520f85aea0c34ae95e00bf7e36e33
Renato de Andrade <natores.andrade@gmail.com>
2022-11-19 23:04:47 -0300

    .

--- operacoes-aritimeticas.py
+++ operacoes-aritimeticas.py
@@ -1,2 +1,4 @@
+#num = int(input('Digite um número inteiro: '))
+#print('O número anterior é {} \ne o número seguinte é {}.'.format(num-1,num+1))
 num = int(input('Digite um número inteiro: '))
-print('O número anterior é {} \ne o número seguinte é {}.'.format(num-1,num+1))
+print(f'O número anterior é {num-1},\ne o número seguinte é {num+1}.')
